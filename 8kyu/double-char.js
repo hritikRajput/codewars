323a3bbb2b9f47d34fae990bc72d83efa937f930
hritikRajput <hritikshivam@gmail.com>
2023-01-15 21:58:06 +0530

    Add a new 8kyu kata

---
+++ 8kyu/double-char.js
@@ -0,0 +1,29 @@
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+const doubleChar = (str) => str.split("").map(c => c + c).join("");
+
+
+
+
+
+function doubleChar(str) {
+	var word = '';
+  for (var i = 0; i < str.length; i++){
+  	word = word + str[i] + str[i];
+  };
+  return word;
+};
+
+
+
+
+
+
+function doubleChar(str) {
+    return str.replace(/(.)/g, "$1$1")
+  }
